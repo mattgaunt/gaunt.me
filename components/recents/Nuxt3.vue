a941e32009408336c52222acc7d1139bfef376ae
Matthew Gaunt <matthew@stormfoundry.co>
2022-10-18 15:51:34 +0100

    chore: recents feed

---
+++ components/recents/Nuxt3.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+const gem1 = ref<HTMLElement>()
+const gem2 = ref<HTMLElement>()
+const gem3 = ref<HTMLElement>()
+const gem4 = ref<HTMLElement>()
+
+const gems = [gem1, gem2, gem3, gem4]
+
+gems.forEach((ref, i) => {
+  useMotion(ref, {
+    initial: {
+      opacity: 0,
+      y: -30,
+      scale: 0.9,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      scale: 1,
+      transition: {
+        type: 'spring',
+        stiffness: 200,
+        damping: 50,
+        delay: 150 * i,
+      },
+    },
+  })
+})
+</script>
+
+<template>
+  <div bg-neutral-900 bg-opacity-25 rounded-12 h-full>
+    <NuxtLink to="/notes" hover="-rotate-1 -translate-y-5" block transition duration-250 h-full>
+      <Card from-opacity-4 to-opacity-6 overflow-hidden p-4>
+        <div flex flex-col h-full gap-4>
+          <div flex-1>
+            <div flex items-center relative>
+              <img ref="gem1" loading="lazy" src="/recents/nuxt/gem-1.svg" absolute left-36 top-38 max-w-46>
+              <img ref="gem2" loading="lazy" src="/recents/nuxt/gem-2.svg" absolute right-12 top-24 max-w-24>
+              <img ref="gem3" loading="lazy" src="/recents/nuxt/gem-3.svg" absolute left-12 top-64 max-w-36>
+              <img ref="gem4" loading="lazy" src="/recents/nuxt/gem-4.svg" absolute left-12 top-8 max-w-60>
+            
+              <div from="#53E8AA/60" block absolute top-30 left-18 glow via-transparent blur-48 w-72 h-72 />
+            </div>
+          </div>
+          <div flex-initial>
+            <div flex justify-center w-full>
+              <div space-y-8 text-left py-8 px-6>
+                <div flex flex-col space-y-3 max-w-lg>
+                  <p text-white text-opacity-75 tracking-wide>
+                    17 Oct
+                  </p> 
+
+                  <h2 title-lg text-white leading-snug>
+                    Creating themes with Nuxt 3
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Card>
+    </NuxtLink>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,35 +4,29 @@ useHead({
 })
 
 const first = ref<HTMLElement>()
-const second = ref<HTMLElement>()
-const third = ref<HTMLElement>()
-const fourth = ref<HTMLElement>()
 const m = ref<HTMLElement>()
 const a = ref<HTMLElement>()
 const g = ref<HTMLElement>()
 const i = ref<HTMLElement>()
 const c = ref<HTMLElement>()
 
-const words = [first, second, third, fourth]
 const letters = [m, a, g, i, c]
 
-words.forEach((ref, i) => {
-  useMotion(ref, {
-    initial: {
-      opacity: 0,
-      y: -20,
+useMotion(first, {
+  initial: {
+    opacity: 0,
+    y: -20,
+  },
+  enter: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 200,
+      damping: 10,
+      delay: 100,
     },
-    enter: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        type: 'spring',
-        stuffiness: 200,
-        damping: 10,
-        delay: 800 * i,
-      },
-    },
-  })
+  },
 })
 
 letters.forEach((ref, i) => {
@@ -58,87 +52,80 @@ letters.forEach((ref, i) => {
 <template>
   <div flex bg-neutral-800 highlight lg:rounded-8 w-full relative>
     <main id="main" relative flex flex-col lg:overflow-y-auto lg:max-h-page w-full>
-      <Container max-w="77.5rem" pt-8 pb-32>
-        <div flex flex-col space-y-8>
+      <Container max-w-7xl pb-32>
+        <div flex flex-col space-y-16 relative>
+          <div absolute ambiance rotate-180>
+            <Magic absolute particles rotate-20 />
+          </div>
+
           <div grid grid-cols-8 gap-24 items-center>
-            <div col-span-5>
-              <div flex flex-col items-start space-y-12 py-12 md:py-24 md:px-12>
-                <div w-full max-w-2xl relative>
-                  <h1 text-7xl text-neutral-500 font-bold leading-20>
-                    <span ref="first" text-white>
-                      Developing with
+            <div col-span-6 col-start-2>
+              <div flex flex-col items-center space-y-16 py-32 relative>
+                <div flex flex-col items-center text-center max-w-172 space-y-10 relative>
+                  <h1 text-76px text-neutral-500 font-bold leading-20>
+                    <span ref="first" text-white transition>
+                      Development, with a little
                       <span text="#F9FD33" inline-flex mr-4>
-                        <span ref="m">M</span>
-                        <span ref="a">a</span>
-                        <span ref="g">g</span>
-                        <span ref="i">i</span>
-                        <span ref="c">c.</span>
+                        <span ref="m" text="#42DC7F">M</span>
+                        <span ref="a" text="#5BE692">a</span>
+                        <span ref="g" text="#6BED9F">g</span>
+                        <span ref="i" text="#7DF4AC">i</span>
+                        <span ref="c" text="#84F7B1">c.</span>
                       </span>
                     </span>
-                    <span ref="second" text-neutral-500 text-opacity-75>And Strong Coffee.</span>&nbsp;
-                    <span ref="third" text-neutral-500 text-opacity-75>And Something.</span>&nbsp;
-                    <span ref="fourth" text-neutral-500 text-opacity-75>Remotely.</span>
                   </h1>
+
+                  <div max-w-40em px-2>
+                    <p text-para text-neutral-400 font-light leading-relaxed tracking-wide>
+                      I’m an all-stack developer, semi-capable designer and full
+                      time tinkerer. If you can’t find me writing code, or designing
+                      in Figma, I’ll likely be crying in AWS console.
+                    </p>
+                  </div>
                 </div>
 
-                <p text-2xl text-neutral-400 leading-relaxed>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                  ut labore et dolore magna aliqua. Quis nostrud exercita ullamco nisi ut
-                  aliquip ex ea commodo consequ. Duis aute irure dolor in reprehen
-                  in voluptate velit esse cillum dolore eu fugiat nulla pariatur<span text-opacity-75>*</span>.
-                </p>
-
-                <NuxtLink
-                  to="/about"
-                  text-lg
-                  font-medium
-                  text-white
-                  tracking-wide
-                  underline
-                  underline-neutral-500
-                  underline-wavy
-                >
-                  Learn more →
-                </NuxtLink>
+                <ButtonLink to="/projects" prefetch>
+                  View Projects
+                </ButtonLink>
               </div>
-            </div>
-            <div col-span-3>
-              <Climbing text-neutral-400 max-w-58 mx-auto />
             </div>
           </div>
           
-          <div grid grid-cols-12 gap-4>
-            <div col-span-7>
-              <div
-                bg-neutral-700
-                highlight
-                rounded-8
-                aspect-square
-              />
+          <div grid grid-cols-8 gap-20>
+            <div col-span-8>
+              <div flex flex-col items-center gap-2>
+                <span text-base text-neutral-400 tracking-wide>
+                  Recent Updates
+                </span>
+
+                <Icon
+                  name="ArrowDown"
+                  size="24"
+                  text-neutral-400
+                />
+              </div>
             </div>
-            <div col-span-5>
-              <div
-                bg-neutral-700
-                highlight
-                rounded-8
-                h-full
-              />
-            </div>
-            <div col-span-5>
-              <div
-                bg-neutral-700
-                highlight
-                rounded-8
-                aspect-square
-              />
-            </div>
-            <div col-span-7>
-              <div
-                bg-neutral-700
-                highlight
-                rounded-8
-                h-full
-              />
+            <div col-span-8>
+              <div grid grid-cols-8 gap-8>
+                <div col-span-3>
+                  <RecentsNuxt3 />
+                </div>
+                <div col-span-5>
+                  <RecentsAlwaysdead />
+                </div>
+                <div col-span-5>
+                  <RecentsStack />
+                </div>
+                <div col-span-3>
+                  <RecentsTweet />
+                </div>
+                <div col-span-3>
+                  <RecentsPolywork />
+                </div>
+                <div col-span-5>
+                  <RecentsLoadout />
+                </div>
+              </div>
             </div>
           </div>
         </div>
