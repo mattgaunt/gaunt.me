ca8052494b56d94024e6a99eca7fe5d038a84889
Matthew Gaunt <matthew@stormfoundry.co>
2022-12-21 12:06:25 +0000

    chore: fix page container

--- components/Page.vue
+++ components/Page.vue
@@ -1,5 +1,5 @@
 <template>
-  <div flex bg-zinc-800 bg-opacity-40 highlight lg:rounded-8 w-full>
+  <div flex bg-zinc-800 bg-opacity-40 highlight rounded-l-8 rounded-r-2 w-full>
     <main id="main" relative flex flex-col lg:overflow-y-auto lg:max-h-page w-full>
       <Container max-w-5xl py-20 lg:py-32>
         <slot />
