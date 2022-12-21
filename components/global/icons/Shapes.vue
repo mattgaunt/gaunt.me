c2a1eb356481fd830a78324316f1cea4b1e7d2ae
Matthew Gaunt <matthew@stormfoundry.co>
2022-12-21 12:21:10 +0000

    chore: update icons

--- components/global/icons/Shapes.vue
+++ components/global/icons/Shapes.vue
@@ -4,12 +4,12 @@
       fill="none"
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M14.5 5.5v-1a2 2 0 0 0-2-2h-8a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h1"
+      d="M2.5 7.5v-4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1ZM11.5 16.5v-4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1Z"
     />
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M5.5 7.5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-8a2 2 0 0 1-2-2v-8Z"
+      d="m2.5 17.5 3-6 3 6h-6ZM14.5 2.5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Z"
     />
   </svg>
 </template>
