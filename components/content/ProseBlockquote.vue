ab9bf3d3e27007256fc8298554f01ef985764b40
Matthew Gaunt <matthew@stormfoundry.co>
2022-12-22 22:38:56 +0000

    chore: prose blockquote

--- components/content/ProseBlockquote.vue
+++ components/content/ProseBlockquote.vue
@@ -1,5 +1,13 @@
 <template>
-  <blockquote>
+  <blockquote
+    bg-zinc-800
+    bg-opacity-50
+    border
+    border-zinc-600
+    border-opacity-25
+    rounded-4
+    px-4
+  >
     <slot />
   </blockquote>
 </template>
