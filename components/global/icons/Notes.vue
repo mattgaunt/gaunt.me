789cdbc6d75c0f33e4a1c8d395300ac023423017
Matthew Gaunt <matthew@stormfoundry.co>
2023-01-05 17:28:34 +0000

    chore: fix svg formatting

--- components/global/icons/Notes.vue
+++ components/global/icons/Notes.vue
@@ -1,14 +1,14 @@
 <template>
-  <svg viewBox="0 0 20 20" aria-hidden="true">
+  <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
     <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke-linecap="round"
+      stroke-linejoin="round"
       d="M2.5 4.5a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2h-11a2 2 0 0 1-2-2v-11Z"
     />
     <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
       fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
       d="M6.5 6.5h7M6.5 13.5h7M6.5 10h7"
     />
   </svg>
--- components/global/icons/Stack.vue
+++ components/global/icons/Stack.vue
@@ -1,14 +1,14 @@
 <template>
-  <svg viewBox="0 0 20 20" aria-hidden="true">
+  <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
     <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
       fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
       d="M14.5 5.5v-1a2 2 0 0 0-2-2h-8a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h1"
     />
     <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke-linecap="round"
+      stroke-linejoin="round"
       d="M5.5 7.5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-8a2 2 0 0 1-2-2v-8Z"
     />
   </svg>
