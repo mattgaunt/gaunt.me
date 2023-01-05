ab25ec07954a452b51e24a02e45361c7cee2889f
Matthew Gaunt <matthew@stormfoundry.co>
2023-01-05 16:02:35 +0000

    chore: reduce pattern skew

--- components/HeroPattern.vue
+++ components/HeroPattern.vue
@@ -15,7 +15,7 @@
             [13, 4],
             [20, 9],
           ]"
-          class="absolute inset-x-0 inset-y-[-50%] h-[200%] w-full skew-y-[-30deg] fill-white/2.5 stroke-white/5 mix-blend-overlay"
+          class="absolute inset-x-0 inset-y-[-50%] h-[200%] w-full skew-y-[-25deg] fill-white/2.5 stroke-white/5 mix-blend-overlay"
         />
       </div>
     </div>
