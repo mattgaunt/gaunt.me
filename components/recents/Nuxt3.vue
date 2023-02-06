5ed684221b82d17e02f873c7d435d1bd29664919
Matthew Gaunt <matthew@stormfoundry.co>
2023-02-06 11:08:19 +0000

    chore: use correct links

--- components/recents/Nuxt3.vue
+++ components/recents/Nuxt3.vue
@@ -30,7 +30,7 @@ gems.forEach((ref, i) => {
 
 <template>
   <div bg-zinc-900 bg-opacity-25 rounded-10 h-full>
-    <NuxtLink to="/notes" hover="-rotate-1 -translate-y-5" block transition duration-250 h-full>
+    <NuxtLink to="/notes/creating-themes-with-nuxt-3" hover="-rotate-1 -translate-y-5" block transition duration-250 h-full>
       <Card min-h-142 overflow-hidden p-4>
         <div flex flex-col h-full gap-4>
           <div flex-1>
