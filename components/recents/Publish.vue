79f7ffe507c7aadea98561d311e94725847a2299
Matthew Gaunt <matthew@stormfoundry.co>
2023-02-06 11:06:55 +0000

    chore: remove motion

--- components/recents/Publish.vue
+++ components/recents/Publish.vue
@@ -1,33 +1,3 @@
-<script lang="ts" setup>
-const gem1 = ref<HTMLElement>()
-const gem2 = ref<HTMLElement>()
-const gem3 = ref<HTMLElement>()
-const gem4 = ref<HTMLElement>()
-
-const gems = [gem1, gem2, gem3, gem4]
-
-gems.forEach((ref, i) => {
-  useMotion(ref, {
-    initial: {
-      opacity: 0,
-      y: -30,
-      scale: 0.9,
-    },
-    visible: {
-      opacity: 1,
-      y: 0,
-      scale: 1,
-      transition: {
-        type: 'spring',
-        stiffness: 200,
-        damping: 50,
-        delay: 150 * i,
-      },
-    },
-  })
-})
-</script>
-
 <template>
   <div bg-zinc-900 bg-opacity-25 rounded-10 h-full>
     <NuxtLink to="/notes" hover="rotate-1 -translate-y-5" block transition duration-250 h-full>
