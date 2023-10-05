1b58ee3e6fae13310fa92aaa21e458b7e63f5b8f
Matthew Gaunt <matthew@stormfoundry.co>
2023-10-05 10:43:39 +0100

    chore: add .env.example

--- components/project/Checkpoint.vue
+++ components/project/Checkpoint.vue
@@ -1,6 +1,6 @@
 <template>
   <div flex bg-zinc-900 bg-opacity-25 rounded-10>
-    <a href="https://checkpoint.live/" target="_blank" hover="-rotate-1 -translate-y-5" transition duration-250>
+    <a href="http://checkpoint.live/" target="_blank" hover="-rotate-1 -translate-y-5" transition duration-250>
       <Card
         from="#168DFF/75!"
         to="#168DFF/100!"
